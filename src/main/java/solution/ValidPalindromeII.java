aa19f93e906875710fa4e325d62158e8c1fde4ad
ander <w22116972@gmail.com>
2025-03-24 17:10:43 +0800

    add 680, 2108

---
+++ src/main/java/solution/ValidPalindromeII.java
@@ -0,0 +1,28 @@
+package solution;
+
+public class ValidPalindromeII {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return isValidPalindrome(s, i + 1, j) || isValidPalindrome(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    private boolean isValidPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
