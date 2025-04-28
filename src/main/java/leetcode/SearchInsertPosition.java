49c99cc4d40b53e6d15b09e7e0acadc4069f775b
Ander <w22116972@gmail.com>
2025-04-28 16:12:54 +0800

    35

--- src/main/java/leetcode/SearchInsertPosition.java
+++ src/main/java/leetcode/SearchInsertPosition.java
@@ -2,30 +2,23 @@ package leetcode;
 
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
-        int head = 0;
-        int tail = nums.length - 1;
-        while (head < tail) {
-            final int mid = (head + tail) / 2;
-            final int midValue = nums[mid];
-            if (target == midValue) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
                 return mid;
-            } else if (target < midValue) {
-                tail--;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             } else {
-                head++;
+                right = mid - 1;
             }
         }
-        if (target == nums[head]) {
-            return head;
-        } else if (target < nums[head]) {
-            if (head == 0) {
-                return 0;
-            } else {
-                return head;
-            }
-        } else {
-            return tail + 1;
-        }
+        return left;
     }
 
     public static void main(String[] args) {
