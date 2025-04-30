3e91579f5b56b5ee45af5ea31afeeee30b06ffeb
ander <w22116972@gmail.com>
2025-04-30 18:52:40 +0800

    226

---
+++ src/main/java/leetcode/Q0226_InvertBinaryTree.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class Q0226_InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
