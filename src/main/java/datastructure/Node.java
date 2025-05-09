de42eb86444f9b6a595e2a2abbb28b3e44eb835f
Ander <w22116972@gmail.com>
2025-05-09 22:40:26 +0800

    146

---
+++ src/main/java/datastructure/Node.java
@@ -0,0 +1,13 @@
+package datastructure;
+
+public class Node {
+    public int key;
+    public int value;
+    public Node prev;
+    public Node next;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
