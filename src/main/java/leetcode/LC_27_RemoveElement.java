164d16d8b16c5b2dab876e1edcb8880bc3e2b791
ander <w22116972@gmail.com>
2025-05-17 13:30:15 +0800

    27

---
+++ src/main/java/leetcode/LC_27_RemoveElement.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class LC_27_RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int nonValIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[nonValIndex] = nums[i];
+                nonValIndex++;
+            }
+        }
+        return nonValIndex;
+    }
+}
