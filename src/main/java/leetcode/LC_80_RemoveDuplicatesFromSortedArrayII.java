7e956d9ad38233bb3bb4bf986b5e544b1270c16d
ander <w22116972@gmail.com>
2025-05-17 13:46:39 +0800

    80

---
+++ src/main/java/leetcode/LC_80_RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+public class LC_80_RemoveDuplicatesFromSortedArrayII {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int count = 1;
+        int prev = nums[0];
+        int index = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == prev) {
+                if (count < 2) {
+                    nums[index] = nums[i];
+                    count++;
+                    index++;
+                }
+            } else {
+                nums[index] = nums[i];
+                prev = nums[i];
+                count = 1;
+                index++;
+            }
+        }
+        return index;
+    }
+}
