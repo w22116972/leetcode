53c9ad681b85694f4c3cb60de4beb756ca2b5b79
ander <w22116972@gmail.com>
2025-06-12 16:38:25 +0800

    150

---
+++ src/main/java/leetcode/LC_150_EvaluateReversePolishNotation.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class LC_150_EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        if (tokens == null || tokens.length == 0) {
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (token.equals("-")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            } else if (token.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (token.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+}
